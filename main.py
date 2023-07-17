230e20232c210b8321120623af0acdeedc3cdb0b
SUJITH KANNAN K H <67060938+Suji-2d@users.noreply.github.com>
2023-07-17 18:28:12 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI, Path
+from pydantic import BaseModel
+from typing import Optional, List, Union
+
+app = FastAPI()
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: Union[bool, None] = None
+
+@app.get("/")
+async def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id: int, q:
+                    Optional[str] = None):
+    return {"item_id": item_id, "q": q}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int, item: Item):
+    return {"item_name": item.name, "item_id": item_id}
